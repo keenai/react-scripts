63b69ef55bb8efe949bfce7d2f7202a8c91c600e
Gabe Hayes <gabe@Gabriels-MacBook-Pro.local>
2017-02-09 15:38:36 -0800

    refactor(bin): Do not use babel-node for script exeuction.

--- bin/keenai-react-scripts.js
+++ bin/keenai-react-scripts.js
@@ -20,28 +20,11 @@ if (script === undefined) {
   process.exit(1);
 }
 
-// Because our scripts are written in ES6, we require the `babel-node` executable.
-// Since it is a dependency of this package, we should first check for it here. If
-// it is unavailable here, we check the bin path relative to the package that is
-// executing the script.
-let babelNodeExecutable;
-[
-  path.resolve(__dirname, process.cwd(), 'node_modules/.bin/babel-node'),
-  path.resolve(__dirname, '../node_modules/.bin/babel-node'),
-].forEach((babelNodePath) => {
-  try {
-    fs.statSync(babelNodePath);
-    babelNodeExecutable = babelNodePath;
-  } catch (e) {
-    // eslint-disable-line no-empty
-  }
-});
-
 switch (script) {
   case 'start': {
     const result = spawn.sync(
-      babelNodeExecutable,
-      [require.resolve(`../scripts/${script}`)].concat(args),
+      'node',
+      [require.resolve(`../build/${script}`)].concat(args),
       { stdio: 'inherit' } // eslint-disable-line comma-dangle
     );
 
