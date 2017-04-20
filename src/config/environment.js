e4fe3f517848d9e99a60866c04f2eda39a23b9b4
Gabe Hayes <gabriel.hayes@gmail.com>
2017-04-19 18:41:38 -0700

    fix(environment): Only export keys on `process` that start with "KEENAI_"

--- src/config/environment.js
+++ src/config/environment.js
@@ -2,15 +2,14 @@
 export default (publicUrl: string) => (
   Object
     .keys(process.env)
+    .filter((key) => key.startsWith('KEENAI_'))
     .reduce(
       (accumulator, key) => ({
         ...accumulator,
         [key]: JSON.stringify(process.env[key]),
       }),
       {
-        NODE_ENV: JSON.stringify(
-          process.env.NODE_ENV || 'development',
-        ),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
         PUBLIC_URL: JSON.stringify(publicUrl),
       },
     )
