9254a98ad30241eefccbcb208e12549e4f1ff96d
Gabe Hayes <gabehayes@users.noreply.github.com>
2017-10-03 15:02:24 -0700

    fix(scripts): Test script should not use commander

--- src/react-scripts/react-scripts-test.js
+++ src/react-scripts/react-scripts-test.js
@@ -3,11 +3,8 @@ import createJestConfig from '../utils/create-jest-config';
 import jest from 'jest-cli';
 import path from 'path';
 import paths from '../config/paths';
-import program from 'commander';
 
 const argv = process.argv.slice(2);
-
-program.parse(process.argv);
 
 // Run the suite with our opinionated config.
 argv.push(
