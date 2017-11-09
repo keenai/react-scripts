b53776e62c161893bdce451e974cc4e396d6d8f2
Gabe Hayes <gabehayes@users.noreply.github.com>
2017-11-09 11:47:24 -0800

    chore(package): upgrade to latest babel

--- src/config/jest/babel-transform.js
+++ src/config/jest/babel-transform.js
@@ -3,12 +3,6 @@ import babelJest from 'babel-jest';
 
 module.exports = babelJest.createTransformer({
   presets: [
-    ['@keenai/keenai', {
-      modules: 'commonjs',
-      targets: {
-        node: 'current',
-      },
-      useBuiltIns: true,
-    }],
+    '@keenai/core',
   ],
 });
